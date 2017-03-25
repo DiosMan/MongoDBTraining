bf08d76ee9da56a27e2d4a49940b437214da9893
Dawei Feng <dawei@daweis-iMac.local>
2017-03-24 23:41:12 -0700

    update operator

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe ('update records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe'});
+    joe = new User({ name: 'Joe', postCount: 0});
 
     joe.save()
       .then(() => done());
@@ -33,5 +33,34 @@ describe ('update records', () => {
   it('a model instance can update', (done) => {
     assertName(joe.update({ name: 'alex'}),done);
 
-  })
+  });
+
+  it('a model class can update', (done) => {
+
+    assertName(
+      User.findOneAndUpdate({ name: 'Joe' }, { name:'alex' }),
+      done
+    );
+
+  });
+
+  it('a model class can update one record', (done) => {
+
+    assertName(
+      User.findByIdAndUpdate(joe._id, { name:'alex' }),
+      done
+    );
+
+  });
+
+  it('A user can have their post count increatment by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => { User.findOne({ name: 'Joe' })
+        .then((user) => {
+          assert(user.postCount === 1);
+          done();
+        });
+    });
+  });
+
 })
