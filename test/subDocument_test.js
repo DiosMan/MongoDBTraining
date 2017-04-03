1741bc3a43f4599b783b10a1672c3d75b7822f44
Dios <David@calvisitor-10-105-158-93.calvisitor.1918.berkeley.edu>
2017-04-02 17:12:53 -0700

    Update Posts of a User

---
+++ test/subDocument_test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Subdocuments', () => {
+  it('can create a subdocument', (done) => {
+    const joe = new User({
+      name:'Joe',
+      posts: [{ title: 'PostTitle' }]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'PostTitle');
+        done();
+      });
+  })
+
+  it('can add subdocument to exsiting users', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    joe.save()
+      // without {} in => function, it will automaticallly return the result of User.findOne
+      .then(() => User.findOne({ name:'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post'});
+        // Inside of {}, we need to return the promise 
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
+      });
+  });
+});
